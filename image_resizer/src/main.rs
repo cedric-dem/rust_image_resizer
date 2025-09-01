0c53843b0fbec35585b37e8ce455ad14dfcfcaf5
cedric <demuelenaere.cedric@gmail.com>
2025-09-01 12:30:24 +0200

    add progress display

--- image_resizer/src/main.rs
+++ image_resizer/src/main.rs
@@ -5,30 +5,52 @@ use std::path::{Path, PathBuf};
 use image::imageops::FilterType;
 
 fn main() {
-    //todo : keep format, set all jpeg, set all jpg, set all png
-    //todo : resize new dimension fixed or proportion of original
-    //todo : if not same ratio : stripes horizontal or vertical + black stripes, or blurred image itself or image simply cropped
-
-    // configuration todo put those in arg
-
+    // configuration
     let input_path = "./input_images";
     let output_path = "./output_images";
 
     let args: Vec<String> = env::args().collect();
     let path = PathBuf::from(input_path);
 
+    println!("==> Start counting");
     match count_images(&path) {
-        Ok((_jpg, _jpeg, _png)) => println!("==> Finished counting"),
+        Ok((jpg, jpeg, png)) => {
+            println!("==> Finished counting");
+            let total = jpg + jpeg + png;
+
+            if args.len() > 1 && args[1] == "--resize" {
+                println!("==> Start resize");
+                println!("currently done 0% of images");
+                if total > 0 {
+                    let mut processed = 0;
+                    let mut next_progress = 10;
+                    match resize_images(
+                        &path,
+                        Path::new(output_path),
+                        800,
+                        600,
+                        &mut processed,
+                        total,
+                        &mut next_progress,
+                    ) {
+                        Ok(_) => {
+                            if next_progress <= 100 {
+                                println!("currently done 100% of images");
+                            }
+                            println!("Images resized to {}", output_path);
+                        }
+                        Err(e) => eprintln!("Failed to resize images: {}", e),
+                    }
+                } else {
+                    println!("currently done 100% of images");
+                }
+                println!("==> Finished resize");
+            }
+        }
         Err(e) => eprintln!("Error traversing {}: {}", path.display(), e),
     }
-
-    if args.len() > 1 && args[1] == "--resize" {
-        match resize_images(&path, Path::new(output_path), 800, 600) {
-            Ok(_) => println!("Images resized to {}", output_path),
-            Err(e) => eprintln!("Failed to resize images: {}", e),
-        }
-    }
 }
+
 fn count_images(dir: &Path) -> std::io::Result<(u64, u64, u64)> {
     let mut count_jpg = 0;
     let mut count_jpeg = 0;
@@ -74,6 +96,9 @@ fn resize_images(
     output: &Path,
     width: u32,
     height: u32,
+    processed: &mut u64,
+    total: u64,
+    next_progress: &mut u64,
 ) -> Result<(), Box<dyn std::error::Error>> {
     if !output.exists() {
         fs::create_dir_all(output)?;
@@ -86,7 +111,15 @@ fn resize_images(
 
             if path.is_dir() {
                 let sub_output = output.join(entry.file_name());
-                resize_images(&path, &sub_output, width, height)?;
+                resize_images(
+                    &path,
+                    &sub_output,
+                    width,
+                    height,
+                    processed,
+                    total,
+                    next_progress,
+                )?;
             } else if path.is_file() {
                 if let Some(ext) = path.extension().and_then(|s| s.to_str()) {
                     let ext = ext.to_lowercase();
@@ -95,6 +128,12 @@ fn resize_images(
                         let resized = img.resize(width, height, FilterType::Lanczos3);
                         let out_path = output.join(path.file_name().unwrap());
                         resized.save(out_path)?;
+                        *processed += 1;
+                        let progress = *processed * 100 / total;
+                        while progress >= *next_progress && *next_progress <= 100 {
+                            println!("currently done {}% of images", *next_progress);
+                            *next_progress += 10;
+                        }
                     }
                 }
             }
